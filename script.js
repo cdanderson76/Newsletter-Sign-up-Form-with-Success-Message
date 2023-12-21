d1fa0e5c7d720ef69c8d9a6e75ce3679f9c3e879
cdanderson76 <138369806+cdanderson76@users.noreply.github.com>
2023-12-21 16:52:36 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -4,15 +4,19 @@ document.addEventListener('DOMContentLoaded', function () {
     
     const form = document.querySelector('form');
     const email = form.querySelector('input[type="email"]');
+    const inputField = document.querySelector('input');
+    const invalidRd = document.querySelector('.invalid');
     
     // console.log(form);
     // console.log(email);
     
     form.addEventListener('submit', function (event) {
     event.preventDefault();
+
     const validEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if ( ! validEmail.test( email.value ) ) {
-        form.classList.add( 'invalid' );
+
+    if ( ! validEmail.test(email.value)) {
+        form.classList.add('invalid');
     } else {
         form.classList.remove('invalid')
         form.submit()
